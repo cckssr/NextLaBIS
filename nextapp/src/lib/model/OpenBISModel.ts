3114d907c08606253ba99370fb9920edd23921dc
cckssr <cedric.kessler@me.com>
2026-01-18 19:36:02 +0100

    feat: more Openbis masterdata definitions

--- nextapp/src/lib/model/OpenBISModel.ts
+++ nextapp/src/lib/model/OpenBISModel.ts
@@ -5,10 +5,11 @@
 export type OpenbisEntityKind =
   | "SPACE"
   | "PROJECT"
-  | "COLLECTION"
-  | "OBJECT" // sample
-  | "EXPERIMENT"
+  | "COLLECTION" // old: experiment
+  | "OBJECT" // old: sample
   | "DATASET";
+
+export type OpenbisDatasetKind = "Physical" | "Link";
 
 // TODO: fully implement OpenBIS types and extensions for each type
 // FEAT: add zod schemas for validation of json-rpc output
@@ -16,9 +17,74 @@ export interface OpenbisEntityBase {
   kind: OpenbisEntityKind;
   permId: string;
   code: string;
-  description?: string | null;
-  registrationDate?: number | null; // epoch ms
-  modificationDate?: number | null; // epoch ms
-  registrator?: string | null;
+  registrationDate: number; // epoch ms
+  modificationDate: number; // epoch ms
+  registratedBy: string;
   frozen?: boolean;
+  description?: string | null;
+}
+
+export interface OpenbisSpace extends OpenbisEntityBase {
+  kind: "SPACE";
+  frozenForProjects?: boolean;
+  frozenForObjects?: boolean;
+}
+
+export interface OpenbisProject extends OpenbisEntityBase {
+  kind: "PROJECT";
+  modifiedBy: string;
+  leader?: string | null;
+  frozenForCollections?: boolean;
+  frozenForObjects?: boolean;
+}
+
+export interface OpenbisCollection extends OpenbisEntityBase {
+  kind: "COLLECTION";
+  modifiedBy: string;
+  projectIdentifier: string;
+  collectionType: string;
+  tags?: string[];
+  frozenForObjects?: boolean;
+  frozenForDatasets?: boolean;
+}
+
+export interface OpenbisObject extends OpenbisEntityBase {
+  kind: "OBJECT";
+  modifiedBy: string;
+  projectIdentifier: string;
+  collectionIdentifier: string;
+  objectType: string;
+  container?: string | null;
+  components?: string[] | null;
+  parentsIdentifiers?: string[] | null;
+  childrenIdentifiers?: string[] | null;
+  tags?: string[];
+  frozenForDatasets?: boolean;
+  frozenForParents?: boolean;
+  frozenForChildren?: boolean;
+  frozenForComponents?: boolean;
+}
+
+export interface OpenbisDataset extends OpenbisEntityBase {
+  kind: "DATASET";
+  logicalKind: OpenbisDatasetKind;
+  modifiedBy: string;
+  accessDate: number; // epoch ms
+  dataProducedBy: string;
+  dataProductionDate: number; // epoch ms
+  datastoreCode: string;
+  measured: boolean;
+  postRegistrationProcessingDone: boolean;
+  collectionIdentifier?: string | null;
+  objectIdentifier: string;
+  datasetType: string;
+  parentsIdentifiers?: string[] | null;
+  childrenIdentifiers?: string[] | null;
+  componets?: string[] | null;
+  containers?: string[] | null;
+  tags?: string[];
+  frozenForParents?: boolean;
+  frozenForChildren?: boolean;
+  frozenForComponents?: boolean;
+  frozenForContainers?: boolean;
 }
