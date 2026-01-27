5d5c304f50482592524bb82b4d15b9e85bfd133c
cckssr <cedric.kessler@me.com>
2026-01-27 20:26:11 +0100

    feat: add new pages for creation, inventory overview, advanced search, settings, and user management

---
+++ nextapp/src/app/(misc)/create/page.tsx
@@ -0,0 +1,15 @@
+import { Title } from "@mantine/core";
+
+/**
+ * Page for creating new entities at various levels.
+ * To be implemented.
+ *
+ * The page will provide a user interface for creating new spaces, projects,
+ * collections, or objects. The base insertion point can be selected.
+ * Main idea is to have a form, where users can input necessary details and
+ * create new objects in a streamlined manner. Maybe even with sequences or
+ * other template functions.
+ */
+export default function CreationPage() {
+  return <Title order={1}>Creation Page - To be implemented</Title>;
+}
---
+++ nextapp/src/app/(misc)/settings/page.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@mantine/core";
+
+/**
+ * Page for application settings
+ * To be implemented.
+ *
+ * The page will provide a user interface for viewing and modifying the general
+ * openbis settings. All settings from the current UI should be available here.
+ * Also, the type creation and management can be integrated here.
+ */
+export default function SettingsPage() {
+  return <Title order={1}>Settings Page - To be implemented</Title>;
+}
